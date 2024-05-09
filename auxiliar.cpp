87a4c660156e53eaf2007a5fbde89464737df964
Sebastián Alonso Burgueño Merino <138322374+Sulkyy1@users.noreply.github.com>
2024-05-09 15:46:12 -0400

    Altura
    Se agrega función auxiliar para calcular la altura en un nodo.

---
+++ auxiliar.cpp
@@ -0,0 +1,20 @@
+include <mtree.hpp>
+
+int altura(Nodo *n) {
+	if (n == nullptr) {
+		return 0;
+	}
+	alturaMax = 0;
+	for (const auto& entrada : entradas) {
+		int h = 0;
+		Entrada *actual = entrada;
+		while (actual != nullptr) {
+			h++;
+			actual = actual -> a;
+		}
+		if (h > alturaMax) {
+			alturaMax = h;
+		}
+	}
+	return alturaMax + 1;
+}

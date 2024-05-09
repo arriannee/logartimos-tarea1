f52d585f836f0c4b681c4b6ba21b32de321823d0
Sebastián Alonso Burgueño Merino <138322374+Sulkyy1@users.noreply.github.com>
2024-05-09 15:53:34 -0400

    Cambios altura
    Se añaden comentarios a la función auxiliar altura

--- auxiliar.cpp
+++ auxiliar.cpp
@@ -1,20 +1,25 @@
 #include "mtree.hpp"
 
 int altura(Nodo *n) {
+	// Si el nodo está vacío no tiene altura
 	if (n == nullptr) {
 		return 0;
 	}
 	alturaMax = 0;
+	// Se revisan las alturas de cada entrada dentro de las entradas del nodo
 	for (const auto& entrada : entradas) {
 		int h = 0;
 		Entrada *actual = entrada;
+		// Se revisa cada hijo de la entrada, por cada uno se suma 1 a la altura
 		while (actual != nullptr) {
 			h++;
 			actual = actual -> a;
 		}
+		// Se elige la mayor cadena de entradas posible
 		if (h > alturaMax) {
 			alturaMax = h;
 		}
 	}
+	// Se devuelve la cadena más larga encontrada +1 (el propio nodo)
 	return alturaMax + 1;
 }
